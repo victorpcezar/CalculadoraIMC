eab38880b81bf38bc3e6e4770e6f0a4c317c0f62
Maduuk <93459614+Maduuk@users.noreply.github.com>
2022-02-06 18:06:31 -0300

    Update script.js

--- script.js
+++ script.js
@@ -8,32 +8,24 @@ function calcular() {
         resultado.innerHTML = `Sem dados! impossivel calcular IMC`
   
     } else {
-        if (imc <= 18.5) {
+        if (imc < 18.5) {
             resultado.innerHTML = `IMC Igual a: ${imc} são consideradas como abaixo do peso ideal.`
-        } else {
-            if (imc <= 24.9) {
-                resultado.innerHTML = `IMC Igual a: ${imc} são consideradas como peso ideal.`   
-            } else {
-                if (imc <= 29.9) {
-                   resultado.innerHTML = `IMC Igual a: ${imc} são consideradas como sobrepeso` 
-                } else {
-                    if (imc <= 34.9) {
-                        resultado.innerHTML = `IMC Igual a: ${imc} são consideradas como obesidade grau I`
-                    } else {
-                        if (imc <= 39.9) {
-                            resultado.innerHTML = `IMC Igual a: ${imc} são consideradas como obesidade grau II`
-                        } else {
-                            if (imc > 40) {
-                                resultado.innerHTML = `IMC Igual a: ${imc} são consideradas como obesidade grau III mórbida`
-                            }
-                        }
-                    }
-                }
-            }
+        } else if (imc >= 18.5 && imc <= 24.9) {
+            resultado.innerHTML = `IMC Igual a: ${imc} são consideradas como peso ideal.`
+        } else if (imc >= 25.0 && imc <= 29.9) {
+            resultado.innerHTML = `IMC Igual a: ${imc} são consideradas como sobrepeso.`
+        } else if (imc >= 30 && imc <= 39.9) {
+            resultado.innerHTML = `IMC Igual a: ${imc} são consideradas como obesidade grau I`
+        } else if (imc >= 30 && imc <= 39.9) {
+            resultado.innerHTML = `IMC Igual a: ${imc} são consideradas como obesidade grau II`
+        } else if (imc > 40) {
+            resultado.innerHTML = `IMC Igual a: ${imc} são consideradas como obesidade grau III ou mórbida`
         }
     }
         
-    
-               
+            
+       
+              
+            
 } 
 

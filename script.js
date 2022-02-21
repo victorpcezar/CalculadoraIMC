ca9d2bcd57309d32697ee52f8e518813336e75e0
Maduuk <93459614+Maduuk@users.noreply.github.com>
2022-02-21 15:25:28 -0300

    Update script.js

--- script.js
+++ script.js
@@ -7,24 +7,14 @@ function calcular() {
         alert('[ERRO] Preencha os dados!')
   
     } else {
-        if (imc < 18.5) {
-            resultado.innerHTML = `IMC Igual a: ${imc} são consideradas como abaixo do peso ideal.`
-        } else if (imc >= 18.5 && imc <= 24.9) {
-            resultado.innerHTML = `IMC Igual a: ${imc} são consideradas como peso ideal.`
-        } else if (imc >= 25.0 && imc <= 29.9) {
-            resultado.innerHTML = `IMC Igual a: ${imc} são consideradas como sobrepeso.`
-        } else if (imc >= 30 && imc <= 39.9) {
-            resultado.innerHTML = `IMC Igual a: ${imc} são consideradas como obesidade grau I`
-        } else if (imc >= 30 && imc <= 39.9) {
-            resultado.innerHTML = `IMC Igual a: ${imc} são consideradas como obesidade grau II`
-        } else {
-            resultado.innerHTML = `IMC Igual a: ${imc} são consideradas como obesidade grau III ou mórbida`
-        }
-    }
-        
-            
-       
-              
-            
+        if (imc < 18.5) {resultado.innerHTML = `IMC Igual a: ${imc} são consideradas como abaixo do peso ideal.`}
+         else if (imc >= 18.5 && imc <= 24.9) {resultado.innerHTML = `IMC Igual a: ${imc} são consideradas como peso ideal.`}
+         else if (imc >= 25.0 && imc <= 29.9) {resultado.innerHTML = `IMC Igual a: ${imc} são consideradas como sobrepeso.`}
+         else if (imc >= 30 && imc <= 39.9) {resultado.innerHTML = `IMC Igual a: ${imc} são consideradas como obesidade grau I`}
+         else if (imc >= 30 && imc <= 39.9) {resultado.innerHTML = `IMC Igual a: ${imc} são consideradas como obesidade grau II`}
+         else if (imc > 40) {resultado.innerHTML = `IMC Igual a: ${imc} são consideradas como obesidade grau III ou mórbida`}
+    }            
 } 
+
+
 
